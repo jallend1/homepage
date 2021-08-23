d624fc353f2d2901930b4c9d3ef41a966a7c87e9
jallend1 <jallend1@gmail.com>
2021-08-23 13:14:27 -0700

    Adds media query to Navbar for smaller screens

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,25 +8,23 @@ const NavBar = () => {
           Jason D. Allen
         </NavLink>
       </p>
-      <div>
-        <ul className='menu'>
-          <li>
-            <NavLink to='/' className='menu-item' exact>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className='menu-item' to='/projects'>
-              Projects
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/about' className='menu-item'>
-              About
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+      <ul className='menu'>
+        <li>
+          <NavLink to='/' className='menu-item' exact>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className='menu-item' to='/projects'>
+            Projects
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/about' className='menu-item'>
+            About
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
