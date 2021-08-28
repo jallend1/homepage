90f286be930690b6b114e35ccc0a12b5cc5cdf22
jallend1 <jallend1@gmail.com>
2021-08-28 12:27:32 -0700

    Begins scaffolding out projects page and adds Animal Crossing Collectibles page

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,43 @@
+import acnhScreen from '../assets/images/acnh.png';
+import '../styles/projects.css';
 const Projects = () => {
-    return (
-        <h2>Projects</h2>
-    )
-}
+  const projects = [
+    {
+      title: 'Animal Crossing Collectible Guide',
+      image: acnhScreen,
+      git: 'https://github.com/jallend1/acnhreboot',
+      live: ''
+    }
+  ];
+  return (
+    <>
+      <h2>Projects</h2>
+      <div className='project'>
+        {projects.map((project) => (
+          <div>
+            <h3>{project.title}</h3>
+            <div>
+              <img
+                className='projectImage'
+                src={project.image}
+                alt={`Screenshot of ${project.title}`}
+              />
+            </div>
+            <div>
+              <p>
+                View Code on <a href={project.git}>Github</a>
+              </p>
+              {project.live ? (
+                <p>
+                  View <a href={project.live}>Live Site</a>
+                </p>
+              ) : null}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Projects;
