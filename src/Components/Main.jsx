dac2f5b28ced97e3bb0b8f541cb1ab5ede4c9f3e
jallend1 <jallend1@gmail.com>
2021-08-29 19:33:05 -0700

    Decreases font size of Navbar items

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,24 +1,24 @@
-import CodeBlock from './CodeBlock';
+import CodeBlock from "./CodeBlock";
 // import coffee from '../assets/images/coffee.png';
-import coffee from '../assets/images/jason-1.jpg';
-import email from '../assets/images/email.svg';
-import gitHub from '../assets/images/github.svg';
-import linkedIn from '../assets/images/linkedin.svg';
-import twitter from '../assets/images/twitter.svg';
+import coffee from "../assets/images/jason-1.jpg";
+import email from "../assets/images/email.svg";
+import gitHub from "../assets/images/github.svg";
+import linkedIn from "../assets/images/linkedin.svg";
+import twitter from "../assets/images/twitter.svg";
 
 const Main = () => {
   return (
-    <div className='container'>
-      <div id='left'>
-        <div className='image'>
-          <img src={coffee} alt='Jason Allen drinking some coffee in Iceland' />
-          <div className='greeting'>
+    <div className="container">
+      <div className="left">
+        <div className="image">
+          <img src={coffee} alt="Jason Allen drinking some coffee in Iceland" />
+          <div className="greeting">
             <h2>Hello, there.</h2>
             <h1>I'm Jason.</h1>
           </div>
         </div>
       </div>
-      <div className='right'>
+      <div className="right">
         <CodeBlock />
         {/* <div className='contact'>
           <button className='btn'>Explore.</button>
@@ -31,25 +31,25 @@ const Main = () => {
           </h3>
           <h4 className='subtitle'>&lt;/coder&gt;</h4>
         </div> */}
-        <div className='contact'>
+        <div className="contact">
           <h2>Connect</h2>
-          <div className='social'>
-            <div className='social-link'>
-              <a href='http://www.github.com/jallend1/'>
-                <img src={gitHub} alt='GitHub' />
+          <div className="social">
+            <div className="social-link">
+              <a href="http://www.github.com/jallend1/">
+                <img src={gitHub} alt="GitHub" />
               </a>
             </div>
-            <div className='social-link'>
-              <a href='http://linkedin.com/in/jallend1/'>
-                <img src={linkedIn} alt='LinkedIn' />
+            <div className="social-link">
+              <a href="http://linkedin.com/in/jallend1/">
+                <img src={linkedIn} alt="LinkedIn" />
               </a>
             </div>
-            <div className='social-link'>
-              <img src={email} alt='email' />
+            <div className="social-link">
+              <img src={email} alt="email" />
             </div>
-            <div className='social-link'>
-              <a href='http://www.twitter.com/jallend1'>
-                <img src={twitter} alt='twitter' />
+            <div className="social-link">
+              <a href="http://www.twitter.com/jallend1">
+                <img src={twitter} alt="twitter" />
               </a>
             </div>
           </div>
