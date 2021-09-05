2927d2defe26237ecbb3251ff999094ad9449228
jallend1 <jallend1@gmail.com>
2021-09-04 19:09:00 -0700

    Adds additional keys to project objects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,36 +1,42 @@
-import acnhScreen from "../assets/images/acnh.png";
-import taskmanScreen from "../assets/images/taskmanScreen.png";
-import "../styles/projects.css";
+import acnhScreen from '../assets/images/acnh.png';
+import taskmanScreen from '../assets/images/taskmanScreen.png';
+import gitHub from '../assets/images/github.svg';
+import '../styles/projects.css';
 const Projects = () => {
   const projects = [
     {
-      title: "Animal Crossing Collectible Guide",
+      title: 'Animal Crossing Collectible Guide',
       image: acnhScreen,
-      git: "https://github.com/jallend1/acnhreboot",
-      live: "https://frosty-lamport-4bd235.netlify.app/",
+      description:
+        'A little video game thing that I will create a better description of shortly.',
+      uses: ['React', 'JSON', 'API'],
+      git: 'https://github.com/jallend1/acnhreboot',
+      live: 'https://frosty-lamport-4bd235.netlify.app/'
     },
     {
-      title: "TaskMan",
+      title: 'TaskMan',
       image: taskmanScreen,
-      git: "https://github.com/jallend1/taskman",
-      live: "https://epic-bhaskara-890fe0.netlify.app/",
-    },
+      description: 'A task manager that will have a better description shortly',
+      uses: ['React', 'Firebase'],
+      git: 'https://github.com/jallend1/taskman',
+      live: 'https://epic-bhaskara-890fe0.netlify.app/'
+    }
   ];
   return (
     <>
       <h2>Projects</h2>
-      <div className="projects">
+      <div className='projects'>
         {projects.map((project) => (
-          <div className="project" key={project.image}>
+          <div className='project' key={project.image}>
             <h3>{project.title}</h3>
             <div>
               <img
-                className="projectImage"
+                className='projectImage'
                 src={project.image}
                 alt={`Screenshot of ${project.title}`}
               />
             </div>
-            <div className="projectLinks">
+            <div className='projectLinks'>
               <p>
                 View Code on <a href={project.git}>Github</a>
               </p>
