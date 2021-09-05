587a3535a1ce4030192c1598880d20ddcb7ad378
jallend1 <jallend1@gmail.com>
2021-09-05 00:16:59 -0700

    Updates spacing of main and navbar components

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,32 +1,30 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav>
       <p>
-        <NavLink to='/' className='logo' exact activeClassName=''>
+        <NavLink to="/" className="logo" exact activeClassName="">
           Jason D. Allen
         </NavLink>
       </p>
-      <ul className='menu'>
+      <ul className="menu">
         <li>
-          <NavLink to='/' className='menu-item' exact>
-            01 - Home
+          <NavLink to="/" className="menu-item" exact>
+            1. Home
           </NavLink>
         </li>
         <li>
-          <NavLink className='menu-item' to='/projects'>
-            02 - Projects
+          <NavLink className="menu-item" to="/projects">
+            2. Projects
           </NavLink>
         </li>
         <li>
-          <NavLink to='/about' className='menu-item'>
-            03 - About
+          <NavLink to="/about" className="menu-item">
+            3. About
           </NavLink>
         </li>
-        <li className='menu-item cta'>
-          CONTACT
-        </li>
+        <li className="menu-item cta">CONTACT</li>
       </ul>
     </nav>
   );
