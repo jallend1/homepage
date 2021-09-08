a6fa83fa6727dc120ac72aa24c9e8287cc5eac91
jallend1 <jallend1@gmail.com>
2021-09-08 13:09:14 -0700

    Updates projects with icons and more detailed information

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,15 +1,17 @@
+import '../styles/projects.css';
 import acnhScreen from '../assets/images/acnh.png';
 import taskmanScreen from '../assets/images/taskmanScreen.png';
-import gitHub from '../assets/images/github.svg';
-import '../styles/projects.css';
+
 const Projects = () => {
   const projects = [
     {
-      title: 'Animal Crossing Collectible Guide',
+      title: 'An Animal Crossing: New Horizons Creature and Collectible Guide',
       image: acnhScreen,
       description:
-        'A little video game thing that I will create a better description of shortly.',
-      uses: ['React', 'JSON', 'API'],
+        'The COVID-19 pandemic fundamentally altered the landscape of what leisure activities were available to us, but mercifully Nintendo came to the rescue with the release of their latest Animal Crossing installment.',
+      description2:
+        "Using the 'ACNH API', I crafted a React.js application that allows you to keep a checklist of which items you have, and also determine which ones are available based on the current date and time. You're also able scope out all the art, songs, and villagers you might want to recruit to your town.",
+      uses: ['React', 'JSON', 'API', 'Materialize', 'Netlify'],
       git: 'https://github.com/jallend1/acnhreboot',
       live: 'https://frosty-lamport-4bd235.netlify.app/'
     },
@@ -17,7 +19,7 @@ const Projects = () => {
       title: 'TaskMan',
       image: taskmanScreen,
       description: 'A task manager that will have a better description shortly',
-      uses: ['React', 'Firebase'],
+      uses: ['React', 'Firebase', 'Firebase Authenticator', 'Netlify'],
       git: 'https://github.com/jallend1/taskman',
       live: 'https://epic-bhaskara-890fe0.netlify.app/'
     }
@@ -28,24 +30,57 @@ const Projects = () => {
       <div className='projects'>
         {projects.map((project) => (
           <div className='project' key={project.image}>
-            <h3>{project.title}</h3>
-            <div>
+            <header>
+              <div className='projectLinks'>
+                <div className='svg'>
+                  <a href={project.git}>
+                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                      <g data-name='Layer 2'>
+                        <rect width='24' height='24' opacity='0' />
+                        <path d='M16.24 22a1 1 0 0 1-1-1v-2.6a2.15 2.15 0 0 0-.54-1.66 1 1 0 0 1 .61-1.67C17.75 14.78 20 14 20 9.77a4 4 0 0 0-.67-2.22 2.75 2.75 0 0 1-.41-2.06 3.71 3.71 0 0 0 0-1.41 7.65 7.65 0 0 0-2.09 1.09 1 1 0 0 1-.84.15 10.15 10.15 0 0 0-5.52 0 1 1 0 0 1-.84-.15 7.4 7.4 0 0 0-2.11-1.09 3.52 3.52 0 0 0 0 1.41 2.84 2.84 0 0 1-.43 2.08 4.07 4.07 0 0 0-.67 2.23c0 3.89 1.88 4.93 4.7 5.29a1 1 0 0 1 .82.66 1 1 0 0 1-.21 1 2.06 2.06 0 0 0-.55 1.56V21a1 1 0 0 1-2 0v-.57a6 6 0 0 1-5.27-2.09 3.9 3.9 0 0 0-1.16-.88 1 1 0 1 1 .5-1.94 4.93 4.93 0 0 1 2 1.36c1 1 2 1.88 3.9 1.52a3.89 3.89 0 0 1 .23-1.58c-2.06-.52-5-2-5-7a6 6 0 0 1 1-3.33.85.85 0 0 0 .13-.62 5.69 5.69 0 0 1 .33-3.21 1 1 0 0 1 .63-.57c.34-.1 1.56-.3 3.87 1.2a12.16 12.16 0 0 1 5.69 0c2.31-1.5 3.53-1.31 3.86-1.2a1 1 0 0 1 .63.57 5.71 5.71 0 0 1 .33 3.22.75.75 0 0 0 .11.57 6 6 0 0 1 1 3.34c0 5.07-2.92 6.54-5 7a4.28 4.28 0 0 1 .22 1.67V21a1 1 0 0 1-.94 1z' />
+                      </g>
+                    </svg>
+                  </a>
+                </div>
+                {project.live ? (
+                  <div className='svg'>
+                    <a href={project.live}>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                      >
+                        <g>
+                          <path d='m18.5 23h-15c-1.378 0-2.5-1.122-2.5-2.5v-15c0-1.378 1.122-2.5 2.5-2.5h9c.276 0 .5.224.5.5s-.224.5-.5.5h-9c-.827 0-1.5.673-1.5 1.5v15c0 .827.673 1.5 1.5 1.5h15c.827 0 1.5-.673 1.5-1.5v-9c0-.276.224-.5.5-.5s.5.224.5.5v9c0 1.378-1.122 2.5-2.5 2.5z' />
+                        </g>
+                        <g>
+                          <path d='m22.5 9c-.276 0-.5-.224-.5-.5v-6.5h-6.5c-.276 0-.5-.224-.5-.5s.224-.5.5-.5h7c.276 0 .5.224.5.5v7c0 .276-.224.5-.5.5z' />
+                        </g>
+                        <g>
+                          <path d='m10.5 14c-.128 0-.256-.049-.354-.146-.195-.195-.195-.512 0-.707l12-12c.195-.195.512-.195.707 0s.195.512 0 .707l-12 12c-.097.097-.225.146-.353.146z' />
+                        </g>
+                      </svg>
+                    </a>
+                  </div>
+                ) : null}
+              </div>
+              <h3>{project.title}</h3>
+            </header>
+            <div className='mainContent'>
+              <p>{project.description}</p>
+              {project.description2 ? <p>{project.description2}</p> : null}
+            </div>
+            <footer>
+              {project.uses.map((used) => (
+                <div key={used}>{used}</div>
+              ))}
+            </footer>
+            {/* <div>
               <img
                 className='projectImage'
                 src={project.image}
                 alt={`Screenshot of ${project.title}`}
               />
-            </div>
-            <div className='projectLinks'>
-              <p>
-                View Code on <a href={project.git}>Github</a>
-              </p>
-              {project.live ? (
-                <p>
-                  View <a href={project.live}>Live Site</a>
-                </p>
-              ) : null}
-            </div>
+            </div> */}
           </div>
         ))}
       </div>
